d3ac5c2fd9bf20722700481f6cde88c7b526537a
Christian Butcher <chrisb2244@gmail.com>
2020-11-25 10:23:50 +0900

    Add a greeting by name, using the first command-line argument.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,12 @@
-def main():
-    print("Hello World")
+import sys
+
+def main(username):
+    print("Hello " + username)
 
 if __name__ == "__main__":
-    main()
+    username = ""
+    if (len(sys.argv) > 1):
+        username = sys.argv[1]
+    else:
+        username = "World"
+    main(username)
