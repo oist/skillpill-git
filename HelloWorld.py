921d3f4364acea88ebabaeba608f0d8ee046d766
Christian Butcher <chrisb2244@gmail.com>
2020-11-25 10:36:11 +0900

    Add a greeting based on time of day. Allow specifying the time as a command-line argument.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 
-def main():
-    print("Hello World")
+import sys
+import time
+
+def main(timeValue):
+    morningStart = 4
+    eveningStart = 18
+    greeting = ""
+    hour = timeValue.tm_hour
+    if (hour >= morningStart) & (hour < 12):
+        greeting = "Good morning"
+    elif (hour >= 12) & (hour < eveningStart):
+        greeting = "Good afternoon"
+    else:
+        greeting = "Good evening"
+    print(greeting)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        timeValue = time.strptime(sys.argv[1], "%Y_%m_%d-%H:%M")
+    else:
+        timeValue = time.localtime(time.time())
+    main(timeValue)
